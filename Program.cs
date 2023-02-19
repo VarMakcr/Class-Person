4884e00ed648c09dffcb6c5e29808a933aa2d905
VarMakcr <124522232+VarMakcr@users.noreply.github.com>
2023-02-19 19:23:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp2
+{
+    public class Person
+    {
+        public string FullName; public int Age;
+        public Person(string fullname, int age)
+        {
+            FullName = fullname;
+            Age = age;
+        }
+        public Person()
+        {
+            return;
+        }
+
+        public string Talk(string fullname)
+        {
+            string FullName = "Такой-то " + fullname + " говорит";
+            return FullName;
+        }
+        public string Move(string fullname)
+        {
+            string FullName = "Такой-то " + fullname + " говорит";
+            return FullName;
+        }
+        public partial class Program : Person
+        {
+            static void Main(string[] args)
+            {
+                Person Вадим = new Person("Вадим", 8);
+                Person Богдан = new Person();
+                Console.WriteLine(Вадим.Move(Вадим.FullName));
+                Console.WriteLine(Вадим.Talk(Вадим.FullName));
+            }
+        }
+    }
+}
